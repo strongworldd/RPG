bda12111bc8ad0b2178102b0d5e69a74edf488b3
strongworldd <michotkevin271@gmail.com>
2025-02-17 11:43:31 +0100

    remove

--- TestCharacter.ts
+++
@@ -1,7 +0,0 @@
-import Character from "./Character.ts";
-
-const char1=new Character("andré",10,20,40,50,50);
-const char2=new Character("samuel",20,10,30,40,40);
-console.log(char1.attack(char2));
-console.log(char2.heal(2));
-
