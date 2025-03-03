84e413150b2106f33fb0ba414e565eef30cbca39
homerofsteel <lucas.vernageau@ynov.com>
2025-03-03 11:45:08 +0100

    fix classes

--- Character.ts
+++ Character.ts
@@ -1,37 +1,37 @@
 export default class Character {
     name="";
-    physical_attack=0;
-    defense_attack=0;
+    physicalAttack=0;
+    defenseAttack=0;
     speed=0;
-    max_health=0;
-    current_health=0;
+    maxHealth=0;
+    currentHealth=0;
 
-    constructor(name:string,physical_attack:number,defense_attack:number,speed:number,max_health:number,current_health:number) {
+    constructor(name:string,physicalAttack:number,defenseAttack:number,speed:number,maxHealth:number,currentHealth:number) {
         this.name=name;
-        this.physical_attack=physical_attack;
-        this.defense_attack=defense_attack;
+        this.physicalAttack=physicalAttack;
+        this.defenseAttack=defenseAttack;
         this.speed=speed;
-        this.max_health=max_health;
-        this.current_health=current_health;
+        this.maxHealth=maxHealth;
+        this.currentHealth=currentHealth;
     }
 
     attack=(target: Character):string => {
-        target.current_health-=this.physical_attack-target.defense_attack;
-        return this.name +" inflige "+(this.physical_attack-target.defense_attack)+" points de dégat.";
+        target.currentHealth-=this.physicalAttack-target.defenseAttack;
+        return this.name +" inflige "+(this.physicalAttack-target.defenseAttack)+" points de dégat.";
     }
 
     heal=(healnumber: number):string => {
-        if (this.current_health+healnumber>this.max_health) {
-            this.current_health=this.max_health;
+        if (this.currentHealth+healnumber>this.maxHealth) {
+            this.currentHealth=this.maxHealth;
         } else {
-            this.current_health+=healnumber;
+            this.currentHealth+=healnumber;
         }
-        return "Le personnage se soigne "+healnumber+" points de vie et a maintenant "+this.current_health+"points de vie."
+        return "Le personnage se soigne "+healnumber+" points de vie et a maintenant "+this.currentHealth+"points de vie."
     }
 
     revive=(healrevive: number): string=> {
-        if (this.current_health==0){
-            this.current_health=healrevive;
+        if (this.currentHealth==0){
+            this.currentHealth=healrevive;
             return "Le personnage ressuscite avec "+healrevive+"points de vie."
         } else {
             return "Le personnage est mort et vous ne pouvez pas le ressusciter"
