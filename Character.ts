9ae4be9c3772b2ae86b1c9e84d04c7e269380bc4
homerofsteel <lucas.vernageau@ynov.com>
2025-03-03 12:03:01 +0100

    fix classes

--- Character.ts
+++ Character.ts
@@ -6,39 +6,39 @@ export default class Character {
     maxHealth =0;
     currentHealth =0;
 
-    constructor(name:string,physicalAttack:number,defenseAttack:number,speed:number,maxHealth:number,currentHealth:number) {
-        this.name =name;
-        this.physicalAttack =physicalAttack;
-        this.defenseAttack =defenseAttack;
-        this.speed =speed;
-        this.maxHealth =maxHealth;
-        this.currentHealth =currentHealth;
+    constructor(name:string, physicalAttack:number, defenseAttack:number, speed:number, maxHealth:number, currentHealth:number) {
+        this.name = name;
+        this.physicalAttack = physicalAttack;
+        this.defenseAttack = defenseAttack;
+        this.speed = speed;
+        this.maxHealth = maxHealth;
+        this.currentHealth = currentHealth;
     }
 
-    attack =(target: Character) :string => {
-        target.currentHealth -=this.physicalAttack-target.defenseAttack;
-        return this.name +" inflige " +(this.physicalAttack-target.defenseAttack) +" points de dégat.";
+    attack =(target :Character) :string => {
+        target.currentHealth -= this.physicalAttack - target.defenseAttack;
+        return this.name + " inflige " + (this.physicalAttack-target.defenseAttack) + " points de dégat.";
     }
 
     heal =(healnumber :number) :string => {
-        if (this.currentHealth +healnumber >this.maxHealth) {
-            this.currentHealth =this.maxHealth;
+        if (this.currentHealth + healnumber >this.maxHealth) {
+            this.currentHealth = this.maxHealth;
         } else {
-            this.currentHealth +=healnumber;
+            this.currentHealth += healnumber;
         }
-        return "Le personnage se soigne "+healnumber+" points de vie et a maintenant "+this.currentHealth+"points de vie."
+        return "Le personnage se soigne " + healnumber + " points de vie et a maintenant " + this.currentHealth + "points de vie."
     }
 
     revive =(healrevive :number) :string=> {
-        if (this.currentHealth ==0){
-            this.currentHealth =healrevive;
-            return "Le personnage ressuscite avec " +healrevive+"points de vie."
+        if (this.currentHealth == 0){
+            this.currentHealth = healrevive;
+            return "Le personnage ressuscite avec " + healrevive+"points de vie."
         } else {
             return "Le personnage est mort et vous ne pouvez pas le ressusciter"
         }
     }
 
     isAlive() :boolean {
-        return this.currentHealth >0;
+        return this.currentHealth > 0;
     }
 }
