314e9193ea08eb5444c14e4450ba7f2900a6e16e
evalenti <valentin.epaud@ynov.com>
2025-03-03 11:46:48 +0100

    guerrier

---
+++ classCharacters/Guerrier.ts
@@ -0,0 +1,5 @@
+class Guerrier extends Character {
+    constructor(name :string, physicalAttack :number, defenseAttack :number, speed :number, maxHealth :number, currentHealth :number) {
+        super(name, physicalAttack, defenseAttack, speed, maxHealth, currentHealth);
+    }
+}
